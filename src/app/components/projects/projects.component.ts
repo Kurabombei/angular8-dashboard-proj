21e2fa7f7416c63d5da2343adb4eff581dfc1ddf
Volos Ivan <ivan.volos1@gmail.com>
2021-05-05 01:04:24 +0300

    Added a list of projects with mock data and details for selected project.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from "../../interfaces/project";
+import { Projects } from "../../mockdata/mock-projects";
 
 @Component({
   selector: 'app-projects',
@@ -7,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+
+  selectedProject?: Project;
+  onSelect(project: Project): void {
+    this.selectedProject = project;
+  }
+  projects = Projects;
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/mockdata/mock-projects.ts
@@ -0,0 +1,13 @@
+import {Project} from "../interfaces/project";
+export const Projects: Project[] = [
+  { id: 2, name: 'Demodon.com' },
+  { id: 3, name: 'Angular Dashboard' },
+  { id: 4, name: 'Cinema Net (database practice)' },
+  { id: 5, name: 'To-Do App' },
+  { id: 6, name: 'Portfolio App' },
+  { id: 7, name: 'Dyplomna robota' },
+  { id: 8, name: 'Work and work and work' },
+  { id: 9, name: 'Selling merchandise' },
+  { id: 10, name: 'Where does Great Egret travel to ?' },
+  { id: 11, name: 'Cool effects with SCSS' }
+];
